48dda030af1aefdaec03f99cd89ce96397985543
INOUE Takuya <inouetakuya5@gmail.com>
2014-12-27 22:25:03 +0900

    Redis client flush all before each spec

--- ruby/chapter01.rb
+++ ruby/chapter01.rb
@@ -1,2 +1,6 @@
 def article_vote(client)
 end
+
+def post_article(client, user, title, link)
+  article_id = client.incr('article:')
+end
--- ruby/spec/chapter01_spec.rb
+++ ruby/spec/chapter01_spec.rb
@@ -4,7 +4,19 @@ require './chapter01'
 describe 'chapter01' do
   let(:client) { Redis.new }
 
+  before do
+    client.flushall
+  end
+
   describe '#article_vote' do
     it { expect(article_vote(client)).to be_nil }
+  end
+
+  describe '#post_article' do
+    let(:user) { 'username' }
+    let(:title) { 'A title' }
+    let(:link) { 'http://www.google.com' }
+
+    it { expect(post_article(client, user, title, link)).to eq 1 }
   end
 end
