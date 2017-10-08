c3335bb9cc313d2d99095c8555168e0b200c5541
Lawrence Pan <lawrencefeipan@gmail.com>
2017-10-08 11:22:20 -0400

    section 4.4.2: list_item

--- ruby/chapter04.rb
+++ ruby/chapter04.rb
@@ -5,12 +5,34 @@ module Chapter4
       init_db(users)
     end
 
-    def market
-      @conn.zrange("market", 0, -1, with_scores: true)
+    # =============== Section 4.4.2 ================== #
+    def list_item(item, seller_id, price)
+      u = User.new(seller_id)
+      @conn.watch(u.inventory_id) do
+        if @conn.sismember(u.inventory_id, item)
+          @conn.multi do |multi|
+            multi.srem(u.inventory_id, item)
+            multi.zadd("market", price, u.item_id(item))
+          end
+        else
+          @conn.unwatch
+        end
+      end
     end
+    # ================================================ #
 
     def user(id)
-      @conn.hgetall("user:#{id}")
+      u = User.new(id)
+      @conn.hgetall u.id
+    end
+
+    def inventory(id)
+      u = User.new(id)
+      @conn.smembers u.inventory_id
+    end
+
+    def market
+      @conn.zrange("market", 0, -1, with_scores: true)
     end
 
     private
@@ -22,8 +44,27 @@ module Chapter4
     end
 
     def init_user(id:, name:, fund:, items:)
-      @conn.hmset("user:#{id}", "name", name, "fund", fund)
-      items.each { |item| @conn.sadd("inventory:#{id}", item) }
+      u = User.new(id)
+      @conn.hmset(u.id, "name", name, "fund", fund)
+      items.each { |item| @conn.sadd(u.inventory_id, item) }
+    end
+  end
+
+  class User
+    def initialize(id)
+      @id = id
+    end
+
+    def id
+      "user:#{@id}"
+    end
+
+    def inventory_id
+      "inventory:#{@id}"
+    end
+
+    def item_id(item)
+      "#{item}.#{@id}"
     end
   end
 end
--- ruby/spec/chapter04_spec.rb
+++ ruby/spec/chapter04_spec.rb
@@ -29,10 +29,44 @@ describe 'Chapter 4' do
   it "initalizes DB" do
     c = Chapter4::Client.new(conn, [user0, user1])
 
-    expect(c.user(0) == {"name"=>"Jon", "fund"=>"100"}).to be_truthy
-    expect(c.user(1) == {"name"=>"Arya", "fund"=>"300"}).to be_truthy
+    expect(c.user(0)).to eq({"name"=>"Jon", "fund"=>"100"})
+    expect(c.user(1)).to eq({"name"=>"Arya", "fund"=>"300"})
 
-    expect(c.inventory(0) == ["Longclaw", "Ghost"]).to be_truthy
-    expect(c.inventory(1) == ["Needle", "Frey Pies"]).to be_truthy
+    expect(c.inventory(0)).to eq(["Longclaw", "Ghost"])
+    expect(c.inventory(1)).to eq(["Needle", "Frey Pies"])
+  end
+
+  # Section 4.4.2
+  it "lists item" do
+    c = Chapter4::Client.new(conn, [user0, user1])
+    expect(c.market == []).to be_truthy
+
+    c.list_item("Needle", 1, 50)
+    expect(c.inventory(1)).to eq(["Frey Pies"])
+    expect(c.market).to eq([
+      ["Needle.1", 50.0]
+    ])
+
+    c.list_item("Ghost", 0, 150)
+    expect(c.inventory(0)).to eq(["Longclaw"])
+    expect(c.market).to eq([
+      ["Needle.1", 50],
+      ["Ghost.0", 150]
+    ])
+
+    c.list_item("Longclaw", 0, 90)
+    expect(c.inventory(0)).to eq([])
+    expect(c.market).to eq([
+      ["Needle.1", 50],
+      ["Longclaw.0", 90],
+      ["Ghost.0", 150]
+    ])
+
+    c.list_item("sdkljfsdkljfsdlk", 0, 1000)
+    expect(c.market).to eq([
+      ["Needle.1", 50],
+      ["Longclaw.0", 90],
+      ["Ghost.0", 150]
+    ])
   end
 end
