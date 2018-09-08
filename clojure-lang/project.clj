b64a1d5ed8e02c4ae248e4534abdafe2e791f0a9
pardeep <pardeep@helpshift.com>
2018-09-08 22:19:56 +0530

    Update clojure-lang licence
    - Update licence from Eclipse Public License to
      MIT License.

--- clojure-lang/project.clj
+++ clojure-lang/project.clj
@@ -1,8 +1,8 @@
-(defproject clojure-lang "0.1.0-SNAPSHOT"
+(defproject clojure-lang "0.1.0"
   :description "Redis in Action example in Clojure"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/josiahcarlson/redis-in-action"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.taoensso/carmine "2.18.1"]
                  [cheshire "5.8.0"]
