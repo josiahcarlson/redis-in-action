44805186d2f5c34a1bf27c8458fbf87955198f56
Akamitori <akamitori@tutanota.com>
2023-05-24 22:33:34 +0300

    Implemented Chapter 6 in C#

---
+++ c#/Chapter6/Chat/ChatMessage.cs
@@ -0,0 +1,47 @@
+﻿namespace Chapter6.Chat;
+
+public class ChatMessage : IEquatable<ChatMessage> {
+	public long Id { get; }
+	public long Ts { get; }
+	public string Sender { get; }
+	public string? Message { get; }
+
+	public ChatMessage(long id,long ts, string sender, string message) {
+		Id = id;
+		Ts = ts;
+		Sender = sender;
+		Message = message;
+	}
+
+	public bool Equals(ChatMessage? other) {
+		if (ReferenceEquals(null, other)) {
+			return false;
+		}
+
+		if (ReferenceEquals(this, other)) {
+			return true;
+		}
+
+		return Id == other.Id && Ts == other.Ts && Sender == other.Sender && Message == other.Message;
+	}
+
+	public override bool Equals(object? obj) {
+		if (ReferenceEquals(null, obj)) {
+			return false;
+		}
+
+		if (ReferenceEquals(this, obj)) {
+			return true;
+		}
+
+		if (obj.GetType() != GetType()) {
+			return false;
+		}
+
+		return Equals((ChatMessage)obj);
+	}
+
+	public override int GetHashCode() {
+		return HashCode.Combine(Id, Ts, Sender, Message);
+	}
+}
---
+++ c#/Chapter6/FileDistribution/TestCallback.cs
@@ -0,0 +1,18 @@
+﻿namespace Chapter6.FileDistribution;
+
+public class TestCallback : ICallback {
+	private int _index;
+	public readonly List<int> Counts = new();
+
+	public void Callback(string? line){
+		if (line is null){
+			_index++;
+			return;
+		}
+		while (Counts.Count == _index){
+			Counts.Add(0);
+		}
+
+		Counts[_index] += 1;
+	}
+}
